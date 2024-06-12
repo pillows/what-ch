f7c4cc75189976515a370b3dc91400dd570ea14c
Matthew Wong <matthew.wong@outlook.com>
2024-06-12 17:20:27 -0400

    Add null types to store

--- src/store.ts
+++ src/store.ts
@@ -2,12 +2,12 @@ import { Atom, atom } from 'jotai';
 import { python_code } from './constants';
 import { Language } from 'pages';
 
-export const modeAtom: Atom<Language> = atom({
+export const modeAtom: Atom<Language | null> = atom({
   label: 'Python',
   value: 'python'
 });
-export const codeAtom: Atom<string> = atom(python_code);
-export const inputAtom: Atom<string> = atom(``);
-export const outputAtom: Atom<string> = atom(``);
+export const codeAtom: Atom<string | null> = atom(python_code);
+export const inputAtom: Atom<string | null> = atom(``);
+export const outputAtom: Atom<string | null> = atom(``);
 
-export const cppCompilerReadyAtom: Atom<boolean> = atom(false);
+export const cppCompilerReadyAtom: Atom<boolean | null> = atom(false);
