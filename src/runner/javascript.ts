9fd6d55b7aaa31d2fc44975014f55ea10b93077a
Matthew Wong <matthew.wong@outlook.com>
2024-07-12 00:36:44 -0400

    Fixed javascript not allowing multi line output

--- src/runner/javascript.ts
+++ src/runner/javascript.ts
@@ -1,43 +1,79 @@
-import { getQuickJS } from 'quickjs-emscripten';
+// import { getQuickJS } from 'quickjs-emscripten';
+
+// export const runJavascript = async (
+//   code: string,
+//   setOutput: (output: string) => void
+// ) => {
+//   setOutput('');
+//   const QuickJS = await getQuickJS();
+//   const vm = QuickJS.newContext();
+//   const logs: string[] = [];
+//   const logHandle = vm.newFunction('log', (...args) => {
+//     const nativeArgs = args.map(vm.dump);
+//     const output = JSON.stringify(nativeArgs);
+//     setOutput(output);
+//     logs.push(nativeArgs.toString());
+//     setOutput(logs.join('\n'));
+//   });
+//   const consoleHandle = vm.newObject();
+//   vm.setProp(consoleHandle, 'log', logHandle);
+//   vm.setProp(vm.global, 'console', consoleHandle);
+//   consoleHandle.dispose();
+//   logHandle.dispose();
+
+//   const fetchHandle = vm.newFunction('fetch', (urlHandle) => {
+//     const url = vm.getString(urlHandle);
+//     const promise = vm.newPromise();
+
+//     fetch(url)
+//       .then((res) => res.text())
+//       .then((txt) => promise.resolve(vm.newString(txt || '')));
+
+//     promise.settled.then(vm.runtime.executePendingJobs);
+//     return promise.handle;
+//   });
+//   fetchHandle.consume((handle) => vm.setProp(vm.global, 'fetch', handle));
+
+//   const result = vm.evalCode(code);
+//   const promiseHandle = vm.unwrapResult(result);
+
+//   await vm.resolvePromise(promiseHandle);
+//   promiseHandle.dispose();
+//   vm.dispose();
+// };
+
+import { quickJS } from '@sebastianwessel/quickjs'
+import { set } from 'nprogress'
 
 export const runJavascript = async (
   code: string,
   setOutput: (output: string) => void
 ) => {
   setOutput('');
-  const QuickJS = await getQuickJS();
-  const vm = QuickJS.newContext();
-  const logs: string[] = [];
-  const logHandle = vm.newFunction('log', (...args) => {
-    const nativeArgs = args.map(vm.dump);
-    const output = JSON.stringify(nativeArgs);
-    setOutput(output);
-    logs.push(nativeArgs.toString());
-    setOutput(logs.join('\n'));
-  });
-  const consoleHandle = vm.newObject();
-  vm.setProp(consoleHandle, 'log', logHandle);
-  vm.setProp(vm.global, 'console', consoleHandle);
-  consoleHandle.dispose();
-  logHandle.dispose();
 
-  const fetchHandle = vm.newFunction('fetch', (urlHandle) => {
-    const url = vm.getString(urlHandle);
-    const promise = vm.newPromise();
+  // Custom log to capture output
+  const outputLogs: string[] = [];
+  const customLog = (...args: any[]) => {
+    outputLogs.push(args.join(' '));
+  };
 
-    fetch(url)
-      .then((res) => res.text())
-      .then((txt) => promise.resolve(vm.newString(txt || '')));
+  // Create a new function with a custom console object
+  const saferFasterEval = (input: string) => {
+    const customConsole = { log: customLog };
+    const functionBody = `
+      const console = arguments[0];
+      "use strict";
+      ${input}
+    `;
+    return Function(functionBody).call(null, customConsole);
+  };
 
-    promise.settled.then(vm.runtime.executePendingJobs);
-    return promise.handle;
-  });
-  fetchHandle.consume((handle) => vm.setProp(vm.global, 'fetch', handle));
+  try {
+    saferFasterEval(code);
+  } catch (error) {
+    customLog(error.toString());
+  }
 
-  const result = vm.evalCode(code);
-  const promiseHandle = vm.unwrapResult(result);
-
-  await vm.resolvePromise(promiseHandle);
-  promiseHandle.dispose();
-  vm.dispose();
+  // Set output
+  setOutput(outputLogs.join('\n'));
 };
